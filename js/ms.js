381569f812fde8ec8b00f672227d26d578ad90f6
apewiki <zhu1996@yahoo.com>
2015-04-13 13:32:19 -0400

    Search is working

--- js/ms.js
+++ js/ms.js
@@ -1,4 +1,4 @@
-$(function() {	
+$(function() {
 	var initLocations = [
 		{
 			'name':'Central Park',
@@ -37,6 +37,11 @@ $(function() {
 		}
 	];
 
+	var Model = {
+		places: initLocations,
+		search_term: ""
+	}
+
 	var Place = function(data) {
 		this.name = ko.observable(data.name);
 		this.category = ko.observable(data.category);
@@ -45,11 +50,12 @@ $(function() {
 
 	var ViewModel = function() {
 		var self = this;
+		self.search_term = ko.observable("");
 		self.locations = ko.observableArray([]);
 
 		initLocations.forEach(function(loc) {
 			self.locations.push(new Place(loc));
-			
+
 		});
 
 		console.log(self.locations().length);
@@ -57,14 +63,34 @@ $(function() {
 			console.log(loc.name() + ':' +loc.category() + ":"+loc.selected());
 		});
 
-		var searchPlace = function (search_term) {
+		ViewModel.reset = function() {
 			self.locations().forEach(function(loc) {
-				if (loc.name().search(search_term) === -1) {
-					loc.selected(false);
-				}
+				loc.selected(true);
 			});
-			//Use visible binding!!!
 		};
+
+		ViewModel.searchPlace = function (search_term) {
+			console.log("In Search:"+search_term);
+			var re = new RegExp(search_term, "i");
+
+			if (search_term.length>0) {
+				self.locations().forEach(function(loc) {
+					if (loc.name().search(re) === -1) {
+						loc.selected(false);
+					}
+				});
+				//Use visible binding!!!
+			} else {
+				ViewModel.reset();
+			}
+
+		};
+
+		ViewModel.setSearchTerm = function(search_term) {
+			self.search_term(search_term);
+			console.log("In setSearchTerm:"+self.search_term());
+		}
+
 
 		var markOnMap = function() {
 
@@ -72,6 +98,33 @@ $(function() {
 	};
 
 	ko.applyBindings(new ViewModel());
+
+	var AppView =  {
+
+
+
+		init: function() {
+			document.getElementById("search_term").addEventListener("keyup", AppView.getSearchTerm);
+			$("#search_form").submit(function(e) {
+				var term = document.getElementById("search_term").value;
+				console.log("submit is called:"+term);
+				ViewModel.setSearchTerm(term);
+				e.preventDefault();
+			})
+		},
+
+		getSearchTerm: function(e) {
+			var term = document.getElementById("search_term").value;
+			console.log("getSearchTerm:"+term);
+			ViewModel.searchPlace(term);
+			e.preventDefault();
+		}
+
+
+
+	};
+
+	AppView.init();
 });
 
 
